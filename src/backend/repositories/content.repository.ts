11b268e80685e77caa136728d859ec342ecdb5d4
codewithprince01 <codewithprince0@gmail.com>
2026-09-11 14:10:42 +0530

    feat: create ContentRepository for static_page_contents table

--- src/backend/repositories/content.repository.ts
+++ src/backend/repositories/content.repository.ts
@@ -5,12 +5,3 @@ export class ContentRepository extends BaseRepository<any> {
     super('static_page_contents');
   }
 
-  async findByPage(page: string, position?: string) {
-    return this.model.findMany({
-      where: {
-        page_name: page,
-        ...(position && { position }),
-      },
-    });
-  }
-}
